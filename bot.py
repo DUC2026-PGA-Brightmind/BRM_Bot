f0946017a71daf19c0a0f49db31fe61f48ff3c0a
hampitou <hampitou007@gmail.com>
2026-06-22 09:05:44 +0700

    Add bot script

---
+++ bot.py
@@ -0,0 +1,16 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+TOKEN = "8024772718:AAH_HzQ4r7QBVSdKXaK3xRCN4Q7-jyX1edw"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Welcome to Bright Mind")
+
+def main():
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    print("Bot is running...")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
